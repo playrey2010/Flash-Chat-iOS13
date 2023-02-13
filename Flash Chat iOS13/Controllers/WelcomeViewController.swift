d78b0eadfef44bba46419a759cfd1700fbd06f74
Reynaldo Aponte <playrey2010@gmail.com>
2023-02-13 15:49:02 -0800

    Added first CocoaPods pod, ready for Firebase installation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -5,18 +5,37 @@
 //  Created by Angela Yu on 21/10/2019.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
-
+import CLTypingLabel
 import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        delayTitleEffect()
+        
     }
     
+    
+    
+    func delayTitleEffect() {
+        titleLabel.text = "⚡️FlashChat"
+        
+        
+        
+        
+//        titleLabel.text = ""
+//        var charIndex = 0
+//        let titleText = "⚡️FlashChat"
+//        for char in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
+//                self.titleLabel.text?.append(char)
+//            }
+//            charIndex += 1
+//        }
+    }
 
 }
